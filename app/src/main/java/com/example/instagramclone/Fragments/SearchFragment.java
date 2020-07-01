be5082ceb622a659b934e8be89fb18d9c173bb5f
GREENTECK9810 <vishal.waliyan47@gmail.com>
2020-07-01 10:24:43 +0530

    search fragment designed with tablayout

--- app/src/main/java/com/example/instagramclone/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/instagramclone/Fragments/SearchFragment.java
@@ -2,22 +2,70 @@ package com.example.instagramclone.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.instagramclone.R;
+import com.example.instagramclone.adaptors.SectionPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class SearchFragment extends Fragment {
-    
+
+    View myFragment;
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_search, container, false);
+
+        viewPager = myFragment.findViewById(R.id.viewpager);
+        tabLayout = myFragment.findViewById(R.id.tablayout);
+
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getChildFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        adapter.addFragment(new SearchAccountFragment(), "ACCOUNT");
+        adapter.addFragment(new SearchHashtagFragment(), "TAGS");
+
+        viewPager.setAdapter(adapter);
     }
 }
